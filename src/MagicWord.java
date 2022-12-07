7ac9ade1b2b52c15521baeb8ceafadf723933116
Eric Sans <esansvi@gmail.com>
2022-12-07 22:02:33 +0100

    done

--- src/MagicWord.java
+++ src/MagicWord.java
@@ -1,2 +1,52 @@
-package PACKAGE_NAME;public class MagicWord {
+public class MagicWord {
+    private String word;
+    private boolean[] found;
+
+    public MagicWord (String word){
+        int i;
+        this.found = new boolean[word.length()];
+        this.word = word;
+        for (i=0; i<word.length();i++){
+            this.found[i] = false;
+        }
+
+    }
+
+    public void printMagicWord (){
+        int i;
+        for (i=0;i<this.word.length();i++){
+            if (this.found[i]==true){
+                System.out.print(this.word.charAt(i));
+            } else {
+                System.out.print("_");
+            }
+        }
+        System.out.println();
+
+        }
+
+    public String getWord() {
+        return word;
+    }
+    public boolean[] getFound() {
+        return found;
+    }
+
+    public int getLength(){
+        return this.word.length();
+    }
+
+    public boolean[] getBoolean(){
+        return this.found;
+    }
+
+    @Override
+    public String toString() {
+        return word;
+    }
 }
+
+
+
+
+
--- src/WordGuess.java
+++ src/WordGuess.java
@@ -1,2 +1,61 @@
-package PACKAGE_NAME;public class WordGuess {
+import org.w3c.dom.ls.LSOutput;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class WordGuess {
+
+    public static void main(String[] args) {
+        int i, j=0;
+        boolean found=false;
+        /* I could ask for the words and build a for, but I will try a preset of 5 words to test the program
+        int numberWords;
+        System.out.println("How many words do you want to play with?");
+        Scanner inputValue;
+        inputValue = new Scanner(System.in);
+        numberWords = inputValue.nextInt();
+        */
+        MagicWord[] Set_words = new MagicWord[5];
+        MagicWord w_one = new MagicWord("hello");
+        Set_words[0]= w_one;
+        MagicWord w_two = new MagicWord("programming");
+        Set_words[1]= w_two;
+        MagicWord w_three = new MagicWord("cat");
+        Set_words[2]= w_three;
+        MagicWord w_four = new MagicWord("alcohol");
+        Set_words[3]= w_four;
+        MagicWord w_five = new MagicWord("vegetables");
+        Set_words[4]= w_five;
+
+        MagicWord chosenWord = Set_words[new Random().nextInt(4)];
+        chosenWord.printMagicWord();
+        //String toPrint = chosenWord.getWord();
+        //System.out.println(toPrint);
+
+        while (!found){
+            j+=1;
+            String input;
+            System.out.println("Introduce a letter or try to guess the whole word:");
+            Scanner inputValue;
+            inputValue = new Scanner(System.in);
+            input = inputValue.nextLine();
+            if (input.length() == 1){
+                for(i=0; i< chosenWord.getLength(); i++){
+                    if (chosenWord.getWord().charAt(i) == input.charAt(0)){
+                        chosenWord.getBoolean()[i] = true;
+                    }
+                }
+                System.out.println("We did " +j + " attempts and the word is: ");
+                chosenWord.printMagicWord();
+            } else {
+                if(input.equals(chosenWord.getWord())){
+                    System.out.println("You got it right! You did it in "+j+" attempts");
+                    found=true;
+                } else {
+                    System.out.println("You got it wrong in your attempt number "+j);
+                }
+            }
+        }
+
+    }
 }
